7c79adcd41497ef677933ddad8094f0079077ba9
thakerngsakdd <thakerngsak.d@ku.th>
2021-01-10 14:10:46 +0700

    update variable flie

---
+++ variable/create.py
@@ -0,0 +1,12 @@
+name = "thakerngsak dumrongmun"
+age = 20
+weight = 95.0
+# boolen | can is has
+can_walk = True
+can_run = False
+
+print(name)
+print(age)
+print(weight)
+print(can_walk)
+print(can_run)
---
+++ variable/get_type.py
@@ -0,0 +1,12 @@
+name = "thakerngsak dumrongmun"
+age = 20
+weight = 95.0
+# boolen | can is has
+can_walk = True
+can_run = False
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_run))
