026231324c12681397975267cf5915a06f87bf81
thakerngsakdd <thakerngsak.d@ku.th>
2021-01-10 14:33:48 +0700

    add variable name file

---
+++ variable/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้ 
+myvar = "Kao"
+my_var = "Kao"
+_my_var = "Kao"
+myVar = "Kao"
+MYVAR = "Kao"
+myvar2 = "Kao"
+
+#ไม่สามารถประกาศได้
+2myvar = "Kao"
+my-var = "Kao"
+my var = "Kao"
