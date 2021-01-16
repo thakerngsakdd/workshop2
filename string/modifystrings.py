db5909d44ac95d0356d55c77af69bfde5b5864cf
thakerngsakdd <thakerngsak.d@ku.th>
2021-01-16 14:25:47 +0700

    add folder string

---
+++ string/modifystrings.py
@@ -0,0 +1,8 @@
+string = "Hello, World!"
+
+print(string.upper())  # Output : " HELLO, WORLD! "
+print(string.lower())  # Output : " Hello, World! "
+print(string.strip())  # Output : "Hello, World!"
+print(string.replace("H", "J"))  # Output : "Jello, World!"
+print(string.split(","))  # Output : [' Hello ', ' World! ']
+print(len(string))  # 15
---
+++ string/stringformat.py
@@ -0,0 +1,6 @@
+age = 26
+txt = "My name is Jim and I am {}"
+
+result = txt.format(age)
+
+print("result", result)  # Output : My name is Jim and I am 26
