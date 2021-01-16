a8c0ebdd9f1f6fb00ca183c65bca2041437bceba
thakerngsakdd <thakerngsak.d@ku.th>
2021-01-16 15:22:02 +0700

    อัพเฉยๆ มันขึ้นมาเอง

--- string/string_exercise_2.py
+++ string/string_exercise_2.py
@@ -2,22 +2,16 @@
 string = "I am the best programmer"
 print(len(string))
 # จงเขียนคำสั่งเพื่อแสดงอักษรแรกของข้อความ "I am the best programmer"
-
 print(string[0])
 # จงเขียนคำสั่งเพื่อแสดง "best" ของข้อความ "I am the best programmer"
-
 print(string[9:13])
 # จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ที่ไม่มี space
-
 print(string.strip())
 # จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ให้เป็นตัวพิมใหญ่ทั้งหมด
-
 print(string.upper())
 # จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ให้เป็นตัวพิมเล็กทั้งหมด
-
 print(string.lower())
 # จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ที่ถูกแทนที่อักษร e ด้วย z ทั้งหมด
-
 print(string.replace("e", "z"))
 # จงเติมคำในช่องว่าเพื่อแสดงชื่อ
 myname = "KLao"
