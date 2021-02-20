38a7da1c8b98945c94cba65be2246e15eccaf8f1
thakerngsakdd <thakerngsak.d@ku.th>
2021-02-20 23:48:04 +0700

    class

---
+++ class/2.py
@@ -0,0 +1,10 @@
+name = "thakerngsak dumrongmun"
+age = 21
+address = "Sisaket"
+
+Person = Person(name, age, address)
+
+print(Person.get_name())
+print(Person.get_age())
+print(Person.get_address())
+print(Person.info())
